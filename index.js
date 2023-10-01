8ee85387efea9e4e8f0f5a75b6d06824b5410090
Praveen Yadav <108298110+Praveen-yad@users.noreply.github.com>
2023-10-01 14:08:33 +0530

    socket link

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const server = app.listen(port, console.log("Server is running.."))
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors:{
-        origin: "http://localhost:3000"
+        origin: "https://echo-2i9m.onrender.com/"
     }
 })
 
